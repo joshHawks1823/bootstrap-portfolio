b5af7c5dbd00da00dce85c52d478c5e0f7faf226
joshhawks1823 <jheat23@gmail.com>
2021-12-05 12:23:00 -0500

    scrollreveal commit

--- JS/index.js
+++ JS/index.js
@@ -2,15 +2,6 @@
 
 ScrollReveal({ reset: true });
 
-ScrollReveal().reveal('.slideDown', {
-  duration: 1500,
-  origin: 'top',
-  distance: '100px',
-  easing: 'cubic-bezier(.37,.01,.74,1)',
-  opacity: 0.3,
-  scale: 0.5,
-});
-
 ScrollReveal().reveal('.scaleUp', {
   duration: 3000,
   scale: 0.85,
